3860d7335325a9abdb4eca7c92e5ee6a8371cb16
Carlos Sanchez <papachoco@gmail.com>
2017-09-17 23:29:37 -0500

    remove

--- src/nti/app/products/courseware_scorm/__init__.py
+++ src/nti/app/products/courseware_scorm/__init__.py
@@ -10,12 +10,3 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from nti.app.products.courseware import MessageFactory
-
-VIEW_EXPORT_COURSE = 'Export'
-VIEW_IMPORT_COURSE = 'Import'
-VIEW_COURSE_EDITORS = 'Editors'
-VIEW_COURSE_INSTRUCTORS = 'Instructors'
-VIEW_COURSE_ADMIN_LEVELS = 'AdminLevels'
-VIEW_ADMIN_IMPORT_COURSE = 'ImportCourse'
-VIEW_COURSE_REMOVE_EDITORS = 'RemoveEditors'
-VIEW_COURSE_REMOVE_INSTRUCTORS = 'RemoveInstructors'
