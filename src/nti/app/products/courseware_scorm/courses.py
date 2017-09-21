e351771741645f57762234d12852d8960e768d4f
Unknown <zinth@me.com>
2017-09-21 10:28:44 -0500

    Add "courses.py" and declare `SCORMCourseInstance`.

---
+++ src/nti/app/products/courseware_scorm/courses.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from nti.contenttypes.courses.courses import CourseInstance
+
+class SCORMCourseInstance(CourseInstance):
+    """
+    An instance of a SCORM course.
+    """
+    pass
