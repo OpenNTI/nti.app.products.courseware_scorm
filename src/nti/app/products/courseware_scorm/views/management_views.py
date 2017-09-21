32b8d68efad20705cddbe48052f477a2020de55c
Unknown <zinth@me.com>
2017-09-21 10:53:41 -0500

    Override `_COURSE_INSTANCE_FACTORY` in `CreateSCORMCourseView`.

--- src/nti/app/products/courseware_scorm/views/management_views.py
+++ src/nti/app/products/courseware_scorm/views/management_views.py
@@ -6,8 +6,11 @@
 
 from nti.app.products.courseware_admin.views.management_views import CreateCourseView
 
+from nti.app.products.courseware_scorm.courses import SCORMCourseInstance
+
 class CreateSCORMCourseView(CreateCourseView):
     """
     An object that can create SCORM courses.
     """
-    pass
+
+    _COURSE_INSTANCE_FACTORY = SCORMCourseInstance
