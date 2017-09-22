ad773a37cd87c7867091241cd6820093d0bd9336
Unknown <zinth@me.com>
2017-09-22 12:11:45 -0500

    Mark `SCORMCourseInstance` as an implementer of `ISCORMCourseInstance`.

--- src/nti/app/products/courseware_scorm/courses.py
+++ src/nti/app/products/courseware_scorm/courses.py
@@ -6,6 +6,10 @@
 
 from nti.contenttypes.courses.courses import CourseInstance
 
+from nti.app.products.courseware_scorm.interfaces import ISCORMCourseInstance
+
+
+@interface.implementer(ISCORMCourseInstance)
 class SCORMCourseInstance(CourseInstance):
     """
     An instance of a SCORM course.
