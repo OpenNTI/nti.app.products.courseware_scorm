82b5ecdc7a9b6e5fbd6dcc85ba571f1f4d9e5984
Unknown <zinth@me.com>
2017-09-22 15:46:12 -0500

    Declare `SCORMCourseMetadata`.

--- src/nti/app/products/courseware_scorm/courses.py
+++ src/nti/app/products/courseware_scorm/courses.py
@@ -7,11 +7,20 @@
 from nti.contenttypes.courses.courses import CourseInstance
 
 from nti.app.products.courseware_scorm.interfaces import ISCORMCourseInstance
+from nti.app.products.courseware_scorm.interfaces import ISCORMCourseMetadata
 
 
 @interface.implementer(ISCORMCourseInstance)
 class SCORMCourseInstance(CourseInstance):
     """
     An instance of a SCORM course.
+    """
+    pass
+
+
+@interface.implementer(IScormCourseMetadata)
+class SCORMCourseMetadata(object):
+    """
+    A metadata object for a SCORM course instance.
     """
     pass
