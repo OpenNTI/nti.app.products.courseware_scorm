28b13dc99faf9fd099de79095255d02d6c94861a
Unknown <zinth@me.com>
2017-09-25 17:25:03 -0500

    Declare the annotation factory `SCORMCourseInstanceMetadata`.

--- src/nti/app/products/courseware_scorm/courses.py
+++ src/nti/app/products/courseware_scorm/courses.py
@@ -4,12 +4,17 @@
 .. $Id$
 """
 
+from zope import component
 from zope import interface
+from zope.annotation.factory import factory as an_factory
 
 from nti.contenttypes.courses.courses import CourseInstance
 
 from nti.app.products.courseware_scorm.interfaces import ISCORMCourseInstance
 from nti.app.products.courseware_scorm.interfaces import ISCORMCourseMetadata
+
+
+SCORM_COURSE_METADATA_KEY = 'nti.app.produts.courseware_scorm.courses.metadata'
 
 
 @interface.implementer(ISCORMCourseInstance)
@@ -19,8 +24,11 @@ class SCORMCourseInstance(CourseInstance):
     """
 
 
+@component.adapter(ISCORMCourseInstance)
 @interface.implementer(IScormCourseMetadata)
 class SCORMCourseMetadata(object):
     """
     A metadata object for a SCORM course instance.
     """
+
+SCORMCourseInstanceMetadata = an_factory(SCORMCourseMetadata, SCORM_COURSE_METADATA_KEY)
