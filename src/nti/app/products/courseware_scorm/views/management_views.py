2b4069abf03256f072d2077dd689784225f4ef9f
Unknown <zinth@me.com>
2017-09-28 12:12:25 -0500

    Declare `UploadSCORMView`.

--- src/nti/app/products/courseware_scorm/views/management_views.py
+++ src/nti/app/products/courseware_scorm/views/management_views.py
@@ -8,6 +8,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
 from nti.app.products.courseware_admin.views.management_views import CreateCourseView
 
 from nti.app.products.courseware_scorm.courses import SCORMCourseInstance
@@ -25,3 +27,9 @@ class CreateSCORMCourseView(CreateCourseView):
     """
 
     _COURSE_INSTANCE_FACTORY = SCORMCourseInstance
+
+
+class UploadSCORMView(AbstractAuthenticatedView):
+    """
+    A view for uploading SCORM zip archives to SCORM courses.
+    """
