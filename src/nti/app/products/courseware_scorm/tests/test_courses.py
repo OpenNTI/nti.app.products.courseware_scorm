cbb4ec20d6f39317493a5a8940600b66521252d9
Carlos Sanchez <papachoco@gmail.com>
2017-11-07 02:21:14 -0600

    organize imports

--- src/nti/app/products/courseware_scorm/tests/test_courses.py
+++ src/nti/app/products/courseware_scorm/tests/test_courses.py
@@ -12,9 +12,9 @@ from hamcrest import none
 from hamcrest import is_not
 from hamcrest import assert_that
 
-from nti.app.products.courseware_scorm.interfaces import ISCORMCourseMetadata
-
 from nti.app.products.courseware_scorm.courses import SCORMCourseInstance
+
+from nti.app.products.courseware_scorm.interfaces import ISCORMCourseMetadata
 
 from nti.app.products.courseware_scorm.tests import CoursewareSCORMLayerTest
 
