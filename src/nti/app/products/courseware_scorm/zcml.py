79203b8d3baab66c5d6d3eaaa251a2a2edababc1
Carlos Sanchez <papachoco@gmail.com>
2017-11-07 02:16:32 -0600

    pylint;pep8

--- src/nti/app/products/courseware_scorm/zcml.py
+++ src/nti/app/products/courseware_scorm/zcml.py
@@ -4,10 +4,9 @@
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from functools import partial
 
@@ -17,15 +16,18 @@ from zope.component.zcml import utility
 
 from zope.configuration import fields
 
-from nti.app.products.courseware_scorm.interfaces import ISCORMCloudClient
 from nti.app.products.courseware_scorm.client import SCORMCloudClient
+
+from nti.app.products.courseware_scorm.interfaces import ISCORMCloudClient
+
+logger = __import__('logging').getLogger(__name__)
 
 
 class IRegisterSCORMCloudClient(interface.Interface):
 
     name = fields.TextLine(title=u"client identifier",
-						   required=False,
-						   default=u'')
+                           required=False,
+                           default=u'')
 
 
 def registerSCORMCloudClient(_context, name=''):
