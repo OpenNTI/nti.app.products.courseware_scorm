38b19c7f7c131b57e261a9df2efb3e4d80bb427f
Unknown <zinth@me.com>
2017-12-15 16:44:50 -0600

    Implement SCORMCloudClient.sync_enrollment_record.

--- src/nti/app/products/courseware_scorm/client.py
+++ src/nti/app/products/courseware_scorm/client.py
@@ -19,6 +19,11 @@ from nti.app.externalization.error import raise_json_error
 
 from nti.app.products.courseware_scorm import MessageFactory as _
 
+from nti.dataserver.users.interfaces import IFriendlyNamed
+from nti.dataserver.users.users import User
+
+from nameparser import HumanName
+
 from nti.app.products.courseware_scorm.interfaces import ISCORMCloudClient
 from nti.app.products.courseware_scorm.interfaces import IScormIdentifier
 from nti.app.products.courseware_scorm.interfaces import ISCORMCourseInstance
@@ -84,3 +89,33 @@ class SCORMCloudClient(object):
         upload_service = self.cloud.get_upload_service()
         cloud_upload_link = upload_service.get_upload_url(redirect_url)
         return hexc.HTTPFound(location=cloud_upload_link)
+
+        # TODO: Upload the zip bytes
+
+        return HTTPFound(location=redirectUrl)
+
+    def sync_enrollment_record(self, enrollment_record, course):
+        """
+        Syncs a course enrollment record with SCORM Cloud.
+        """
+        course_id = IScormIdentifier(course).get_id()
+        user = User.get_user(enrollment_record.Principal.id)
+        learner_id = IScormIdentifier(user).get_id()
+        named = IFriendlyNamed(user)
+        if named and named.realname:
+            human_name = HumanName(named.realname)
+        if not human_name and human_name.first and human_name.last:
+            # TODO: Raise error?
+            return
+        service = self.cloud.get_registration_service()
+        logger.info("""Syncing enrollment record:
+                        courseid=%s
+                        fname=%s
+                        lname=%s
+                        learnerid=%s""",
+                        course_id, human_name.first, human_name.last, learner_id)
+        service.createRegistration(courseid=course_id,
+                                    regid=None,
+                                    fname=human_name.first,
+                                    lname=human_name.last,
+                                    learnerid=learner_id)
