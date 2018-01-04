f10e6bc2808001260159bf89e69542b0b5209fd7
Unknown <zinth@me.com>
2018-01-04 14:17:31 -0600

    Add admin_views.py.

---
+++ src/nti/app/products/courseware_scorm/views/admin_views.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from pyramid import httpexceptions as hexc
+
+from pyramid.view import view_config
+
+from zope import component
