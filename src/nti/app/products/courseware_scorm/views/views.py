624248189eef31a4312fa87f6d189340530c1881
Unknown <zinth@me.com>
2018-01-16 11:15:48 -0600

    Implement LaunchSCORMCourseView.

--- src/nti/app/products/courseware_scorm/views/views.py
+++ src/nti/app/products/courseware_scorm/views/views.py
@@ -10,20 +10,31 @@ from __future__ import absolute_import
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
+from nti.app.products.courseware.interfaces import ICourseInstanceEnrollment
+
+from nti.app.products.courseware_scorm.interfaces import ISCORMCloudClient
+from nti.app.products.courseware_scorm.interfaces import ISCORMCourseMetadata
+from nti.app.products.courseware_scorm.interfaces import IScormIdentifier
+
 from nti.app.products.courseware_scorm.views import LAUNCH_SCORM_COURSE_VIEW_NAME
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 
+from nti.contenttypes.courses.utils import get_enrollment_in_hierarchy
+
 from nti.dataserver import authorization as nauth
 
+from pyramid import httpexceptions as hexc
+
 from pyramid.view import view_config
+
+from zope import component
 
 
 @view_config(route_name='objects.generic.traversal',
              renderer='rest',
              context=ICourseInstance,
              request_method='GET',
-             permission=nauth.ACT_NTI_ADMIN,
              name=LAUNCH_SCORM_COURSE_VIEW_NAME)
 class LaunchSCORMCourseView(AbstractAuthenticatedView):
     """
@@ -31,5 +42,7 @@ class LaunchSCORMCourseView(AbstractAuthenticatedView):
     """
 
     def __call__(self):
-        # TODO: Implement
-        pass
+        client = component.getUtility(ISCORMCloudClient)
+        enrollment = get_enrollment_in_hierarchy(self.context, self.remoteUser)
+        registration_id = str(IScormIdentifier(enrollment).get_id())
+        return hexc.HTTPSeeOther(location=client.launch(registration_id, u'message'))
