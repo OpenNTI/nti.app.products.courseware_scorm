ef098bdcfb0e29ff92596c9097df95f5746b1b1c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-01-22 14:59:46 -0600

    sort imports; add logger

--- src/nti/app/products/courseware_scorm/views/views.py
+++ src/nti/app/products/courseware_scorm/views/views.py
@@ -8,14 +8,17 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-from nti.app.base.abstract_views import AbstractAuthenticatedView
+from pyramid import httpexceptions as hexc
 
-from nti.app.products.courseware.interfaces import ICourseInstanceEnrollment
+from pyramid.view import view_config
+
+from zope import component
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
 
 from nti.app.products.courseware_scorm import MessageFactory as _
 
 from nti.app.products.courseware_scorm.interfaces import ISCORMCloudClient
-from nti.app.products.courseware_scorm.interfaces import ISCORMCourseMetadata
 
 from nti.app.products.courseware_scorm.views import LAUNCH_SCORM_COURSE_VIEW_NAME
 
@@ -27,11 +30,7 @@ from nti.contenttypes.courses.utils import is_course_instructor
 
 from nti.dataserver import authorization as nauth
 
-from pyramid import httpexceptions as hexc
-
-from pyramid.view import view_config
-
-from zope import component
+logger = __import__('logging').getLogger(__name__)
 
 
 @view_config(route_name='objects.generic.traversal',
@@ -45,7 +44,7 @@ class LaunchSCORMCourseView(AbstractAuthenticatedView):
     """
 
     def __call__(self):
-        if get_enrollment_record(self.context, self.remoteUser) is None \
+        if      get_enrollment_record(self.context, self.remoteUser) is None \
             and not is_course_editor(self.context, self.remoteUser) \
             and not is_course_instructor(self.context, self.remoteUser) \
             and not nauth.is_admin_or_site_admin(self.remoteUser):
