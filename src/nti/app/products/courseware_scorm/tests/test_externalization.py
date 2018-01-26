8f12d49a10c4ba772288c2db7a3e1139277fb333
Unknown <zinth@me.com>
2018-01-26 17:42:58 -0600

    Write a test case to make sure externalizations are created for models.

--- src/nti/app/products/courseware_scorm/tests/test_externalization.py
+++ src/nti/app/products/courseware_scorm/tests/test_externalization.py
@@ -8,7 +8,33 @@ from __future__ import absolute_import
 # pylint: disable=protected-access,too-many-public-methods
 
 from hamcrest import is_
+from hamcrest import none
 from hamcrest import is_not
 from hamcrest import has_item
 from hamcrest import not_none
 from hamcrest import assert_that
+
+from nti.app.products.courseware_scorm.interfaces import IScormInstance
+from nti.app.products.courseware_scorm.interfaces import IScormRegistration
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.externalization.externalization import to_external_object
+
+from nti.scorm_cloud.client.registration import Instance
+from nti.scorm_cloud.client.registration import Registration
+
+
+class TestExternal(ApplicationLayerTest):
+
+    def test_scorm_registration(self):
+        mock_instance = Instance(instanceId='instanceId', courseVersion='version', updateDate='updateDate')
+        mock_reg = Registration(appId='appId', registrationId='regId', courseId='courseId',
+                     courseTitle='Title', lastCourseVersionLaunched='lastVersion',
+                     learnerId='learnerId', learnerFirstName='learnerFirst', learnerLastName='learnerLast',
+                     email='email', createDate='createDate', firstAccessDate='firstAccess', lastAccessDate='lastAccess',
+                     completedDate='completedDate', instances=[mock_instance])
+        reg = IScormRegistration(mock_reg)
+        assert_that(reg, is_not(none()))
+        ext_reg = to_external_object(reg)
+        assert_that(ext_reg, is_not(none()))
