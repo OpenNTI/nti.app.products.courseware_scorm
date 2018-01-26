1988332136ef6538d4501257c99ce615551029ae
Unknown <zinth@me.com>
2018-01-26 15:51:00 -0600

    Add text_externalization.py.

---
+++ src/nti/app/products/courseware_scorm/tests/test_externalization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# pylint: disable=protected-access,too-many-public-methods
+
+from hamcrest import is_
+from hamcrest import is_not
+from hamcrest import has_item
+from hamcrest import not_none
+from hamcrest import assert_that
