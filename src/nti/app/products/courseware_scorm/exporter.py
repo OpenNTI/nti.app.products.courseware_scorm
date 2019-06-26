abad2b0f6228f9e9cde644ab17d90a5dc1fdda7d
Josh Zuech <josh.zuech@nextthought.com>
2019-06-26 15:59:00 -0500

    Missing files

---
+++ src/nti/app/products/courseware_scorm/exporter.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+
+from six.moves import cStringIO
+
+from zope import component
+from zope import interface
+
+from nti.app.products.courseware_scorm.interfaces import ISCORMCloudClient
+from nti.app.products.courseware_scorm.interfaces import ISCORMContentInfoContainer
+
+from nti.contenttypes.courses.exporter import BaseSectionExporter
+
+from nti.contenttypes.courses.interfaces import ICourseSectionExporter
+
+from nti.namedfile.file import safe_filename
+
+from nti.scorm_cloud.client.request import ScormCloudError
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@interface.implementer(ICourseSectionExporter)
+class CourseSCORMPackageExporter(BaseSectionExporter):
+    """
+    Export scorm content into a `ScormContent` folder in the export. Each
+    content will have a folder containing it's json data as well as a zip
+    containing the scorm archive.
+    """
+
+    def get_archive(self, client, scorm_content):
+        """
+        Return the scorm content info referenced by the given scorm_id.
+        """
+        try:
+            result = client.get_archive(scorm_content.scorm_id)
+            result = cStringIO(result)
+        except ScormCloudError:
+            logger.exception("Scorm exception while exporting (%s) (%s)",
+                             scorm_content.ntiid, scorm_content.scorm_id)
+            result = None
+        return result
+
+    def export(self, course, filer, backup=True, salt=None):
+        scorm_container = ISCORMContentInfoContainer(course, None)
+        if not scorm_container:
+            return
+        client = component.queryUtility(ISCORMCloudClient)
+        if client is None:
+            logger.warn("Exporting SCORM content without client configured.")
+            return
+        bucket = self.course_bucket(course) or ''
+        filer.default_bucket = os.path.join(bucket, 'ScormContent')
+
+        for scorm_content in scorm_container.values():
+            logger.info("Exporting scorm content (%s) (%s)",
+                        scorm_content.ntiid, scorm_content.scorm_id)
+            scorm_archive = self.get_archive(client, scorm_content)
+            if scorm_archive is not None:
+                content_ntiid = scorm_content.ntiid
+                if not backup:
+                    content_ntiid = self.hash_ntiid(scorm_content.ntiid, salt)
+                folder_name = safe_filename(content_ntiid)
+                bucket = os.path.join(bucket, folder_name)
+                zip_name = getattr(scorm_content.upload_job, 'UploadFilename', content_ntiid)
+                zip_name = safe_filename(zip_name)
+                filer.save(zip_name,
+                           scorm_archive,
+                           overwrite=True,
+                           bucket=bucket,
+                           contentType='application/zip; charset=UTF-8')
+                # We not currently want to copy *any* scorm content state except for ntiid.
+                # All of the other fields will be updated when the archive is uploaded to
+                # scorm cloud.
+                scorm_content_ext = {'NTIID': content_ntiid,
+                                     'ScormArchiveFilename': zip_name}
+                filer.save('scorm_content.json',
+                           scorm_content_ext,
+                           overwrite=True,
+                           bucket=bucket,
+                           contentType="application/json")
+        filer.default_bucket = None # restore
---
+++ src/nti/app/products/courseware_scorm/importer.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+import simplejson
+
+from zope import component
+from zope import interface
+
+from nti.app.products.courseware_scorm.interfaces import ISCORMCloudClient
+
+from nti.app.products.courseware_scorm.utils import upload_scorm_content_async
+
+from nti.cabinet.filer import transfer_to_native_file
+
+from nti.contentlibrary.interfaces import IFilesystemBucket
+
+from nti.contenttypes.courses.importer import BaseSectionImporter
+
+from nti.contenttypes.courses.interfaces import ICourseSectionImporter
+
+from nti.scorm_cloud.client.request import ScormCloudError
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@interface.implementer(ICourseSectionImporter)
+class CourseSCORMPackageImporter(BaseSectionImporter):
+
+    def _get_scorm_content_ext(self, content_bucket):
+        scorm_content_file = content_bucket.getChildNamed('scorm_content.json')
+        if scorm_content_file is not None:
+            scorm_content = scorm_content_file.readContents()
+            scorm_content = scorm_content.decode('utf-8') if isinstance(scorm_content, bytes) else scorm_content
+            return simplejson.loads(scorm_content)
+
+    def process(self, course, filer, writeout=True):
+        path = self.course_bucket_path(course) + 'ScormContent'
+        client = component.queryUtility(ISCORMCloudClient)
+        if client is None:
+            logger.warn("Importing SCORM content without client configured.")
+            return
+        if filer.is_bucket(path):
+            content_buckets = filer.get(path)
+            for content_bucket in content_buckets or ():
+                scorm_content_ext = self._get_scorm_content_ext(content_bucket)
+                if scorm_content_ext is None:
+                    logger.warn("No scorm json found (%s)", content_bucket)
+                    continue
+                scorm_content_ntiid = scorm_content_ext.get('NTIID')
+                scorm_archive_name = scorm_content_ext.get('ScormArchiveFilename')
+                if not scorm_content_ntiid or not scorm_archive_name:
+                    logger.warn("Invalid scorm json (%s)", scorm_content_ext)
+                    continue
+                scorm_archive = content_bucket.getChildNamed(scorm_archive_name)
+                if scorm_archive is None:
+                    logger.warn("No scorm archive found (%s/%s)", content_bucket, scorm_archive_name)
+                    continue
+
+                try:
+                    # Ok, we're good to upldate; ensure we use the ntiid we have
+                    upload_scorm_content_async(scorm_archive, client, ntiid=scorm_content_ntiid)
+                except ScormCloudError:
+                    logger.exception("Scorm exception while uplading (%s)",
+                                     scorm_content_ntiid)
+                else:
+                    # Save source
+                    if writeout and IFilesystemBucket.providedBy(course.root):
+                        scorm_archive = content_bucket.getChildNamed(scorm_archive_name)
+                        if scorm_archive is not None:
+                            self.makedirs(course.root.absolute_path)
+                            new_path = os.path.join(course.root.absolute_path,
+                                                    'ScormContent',
+                                                    scorm_archive_name)
+                            transfer_to_native_file(scorm_archive, new_path)
